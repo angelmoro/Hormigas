a3b9ea4d05ffe2d60ff6210b0d038604276146c5
Angel <Angel@LAPTOP-KNDLJQVO>
2020-08-20 22:06:48 +0200

    adaptacion w10

---
+++ inc/bool.h
@@ -0,0 +1,18 @@
+/*
+ * bool.h
+ *
+ *  Created on: 8 ago. 2020
+ *      Author: Angel
+ */
+
+#ifndef INC_BOOL_H_
+#define INC_BOOL_H_
+
+
+
+#define FALSE 0
+#define TRUE 1
+
+
+
+#endif /* INC_BOOL_H_ */
